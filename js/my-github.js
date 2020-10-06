a6f3f349eb41707c1ae0c3ccb26965853b95dfa6
amarabayor <bayoramara@gmail.com>
2020-10-06 10:22:22 +0200

    Home work JS III week-1

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,55 @@
-// Write code here to communicate with Github
+//Write code here to communicate with Github
+function fetchGithubEndpoint(endpoint) {
+  const githubAPIBaseURL = "https://api.github.com";
+  fetch(`${githubAPIBaseURL}${endpoint}`)
+    .then(function(response) {
+      console.log(response.status);
+      return response.json();
+    })
+    .then(function(repositories) {
+      console.log(repositories);
+      const reposList = document.getElementById("repos-list");
+
+      for (let i = 0; i < repositories.length; i++) {
+        const repository = repositories[i];
+        let a = document.createElement("a");
+        const repositoryElement = document.createElement("li");
+        a.setAttribute("href", repository.html_url);
+        a.innerText = repository.name;
+        repositoryElement.appendChild(a);
+        reposList.appendChild(repositoryElement);
+      }
+    });
+}
+const userName = "amarabayor";
+fetchGithubEndpoint(`/users/${userName}/repos`);
+// window.onload = main;
+// function main() {
+//     let reposList = document.getElementById("repos-list");
+//     reposList.innerHTML = "";
+//     let listOfRepo = getListofRepo("amarabayor");
+//     listOfRepo.then(res => {
+//       res.forEach(item => {
+//         let li = document.createElement("li");
+//         li.innerText = item;
+//         reposList.appendChild(li);
+//       });
+//     });
+// }
+// /**
+//  * Get list of user repository Name
+//  *
+//  * @param {string} user
+//  * @return {Array} List on Repo name
+//  */
+// function getListofRepo(user = "amarabayor") {
+//     return fetch("https://api.github.com/users/" + user + "/repos")
+//       .then(resp => {
+//         return resp.json();
+//       })
+//       .then(data => {
+//         return data.map(item => {
+//           return item.name;
+//         });
+//       });
+// }
